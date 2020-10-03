b1d441bf907112182cabaf82c99b643a885d7b17
Helina Abye <helinaago@gmail.com>
2020-10-03 13:17:44 +0300

    add motivational content, add material ui style

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ import React, { useEffect, useState, useCallback } from 'react';import {
   // faInstagram
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Grid, IconButton, Typography, Link } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { list } from './imgList'
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import 'materialize-css/dist/css/materialize.min.css';
 // import M from "materialize-css";
@@ -22,10 +27,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import ApiContextProvider from './contexts/ApiContext';
 import './app.css';
 
+const useStyles = makeStyles((theme) => ({
+  content: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  arrowIcon: {
+    color: "#fff"
+  }
+}));
+
 function App() {
   // useEffect(() => M.AutoInit(), []);
   const [date, setDate] = useState(null)
   const [tick, setTick] = useState(null)
+  const [index, setIndex] = useState(0)
+  const classes = useStyles();
+  const imgList = list;
+
+  const handleChangeImg = (direction) => {
+    if (direction === "next") {
+      if (index+1 !== imgList.length) {
+        setIndex(index + 1)
+      }
+    } 
+    if (direction === "back") {
+      if (index !==0 ) {
+      setIndex(index - 1)
+      }
+    }
+  }
+
   const getDate = useCallback(
     () => {
       setDate(new Date().toLocaleString("en-US", {timeZone: "Africa/Kampala"}))
@@ -44,22 +77,56 @@ useEffect(() => {
   return (
     <div className="App">
     <header className="App-header">
-       <p>
-       Hello! I am Helina, a Software Developer.
-        </p> 
-       <div class="social-container">
-       <a href="https://www.twitter.com/helinaabye_" className="twitter social">
-          <FontAwesomeIcon icon={faTwitter} size="1.5x" />
-        </a>
-        <a href="https://github.com/helinaabye"
-          className="github social">
-          <FontAwesomeIcon icon={faGithub} size="1.5x" />
-        </a>
-       </div>
-       <p>
-       The current date and time in Ethiopia is
-        </p> 
-       {date}
+      <Grid item container md={10} >
+        <Grid item container sm={12} md={6}>
+          <Grid item container  className={classes.content} xs={12}>
+            <Grid item xs={2}>
+              <IconButton 
+              className={classes.arrowIcon}
+              onClick={() => handleChangeImg("back")}>
+                <ArrowBackIosIcon/>
+              </IconButton>
+            </Grid>
+            <Grid item container xs={8}>
+              <img src={imgList[index]} width="500px" alt="chibird motivation card"/>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton 
+              className={classes.arrowIcon}
+              onClick={() => handleChangeImg("next")}>
+                <ArrowForwardIosIcon/>
+              </IconButton>
+            </Grid>
+          </Grid>
+          <Grid item container   className={classes.content} xs={12}>
+            <Typography  variant="caption">
+              Awesome motivational art from <Link href="https://chibird.com/">Chibird</Link>!
+            </Typography>
+          </Grid>       
+        </Grid>
+        <Grid item container sm={12} md={6}  className={classes.content}>
+          <div>
+            <p>
+              Hello! I am Helina, a Software Developer.
+            </p> 
+            <div  className={classes.content}>
+            <a href="https://www.twitter.com/helinaabye_" className="twitter social">
+              <FontAwesomeIcon icon={faTwitter} size="1x" />
+            </a>
+            <a href="https://github.com/helinaabye"
+              className="github social">
+              <FontAwesomeIcon icon={faGithub} size="1x" />
+            </a>
+           </div>
+           <p>
+            The current date and time in Ethiopia is
+           </p>  
+            {date}
+           <p>“Don't count the days. Make the days count.”<br/> Muhammad Ali</p>
+          </div>
+        </Grid>
+      </Grid>
+     
       {/* <ApiContextProvider>
       <AuthContextProvider>
         <BrowserRouter>
